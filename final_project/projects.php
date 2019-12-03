36386393e10bc2d5ef279fca8004da76cda34a32
Ryan Guelzo <rguelzo@mail.greenriver.edu>
2019-12-03 13:30:45 -0500

    Added more projects

--- final_project/projects.php
+++ final_project/projects.php
@@ -10,10 +10,19 @@ include('includes/header.html');
     <h1>Projects</h1>
     <p> These are the projects that I am currently working on, or have worked on in the past.</p>
 </div>
-<div>
+<div class="project_list">
     <h2>WTIA</h2>
-    <p>I worked on the styling and some of the JS for this site. I was an essential group member and was always ready to
-        work and kept the team on task. </p>
+    <p>I worked on the styling and some of the Javascript for this site. I was an essential group member and was always
+        ready to work and kept the team on task. </p>
+    <h2>Minidoka Pilrimage</h2>
+    <p>I worked on the registration forms, the validation, and some of the Javascript for these. I also worked as the
+        SCRUM master for group.</p>
+    <h2>Quick Trips</h2>
+    <p>I helped to develop the forms for this page as well as set up the original framework for this project.</p>
+    <h2>Offline Projects</h2>
+    <h3>Text-Adventure Game</h3>
+    <p>I began working on a short text adventure game in python that ran for about 3 levels. I then rewrote it in Java
+    for ease of use. The game is available to be played, but is still incomplete.</p>
 </div>
     
     
