ddd641479d8e6b0a28473044d91a871b908ddc9a
Ryan Guelzo <rguelzo@mail.greenriver.edu>
2019-12-03 13:51:16 -0500

    working on positioning the image.

--- final_project/index.php
+++ final_project/index.php
@@ -8,7 +8,7 @@ include('includes/header.html');
 
 <div class="page-header"><h1>Ryan Guelzo</h1></div>
 <p>This is a page about me and the projects that I am working on.</p>
-<img src="img/Ryan1.jpg" alt="This is me." style="width: 50%; height 50%;">
+<img src="img/Ryan1.jpg" alt="This is me." style=" position: relative; alignment: left ; width: 50%; height 50%;">
 
 <p>Are you looking for a go-getter with a great attitude? Do you want someone who is great at communicating new ideas
 and keeping teams up to date?</p>
@@ -17,7 +17,9 @@ and keeping teams up to date?</p>
     improve things with my training with Agile and Scrum. I am great at keeping people on task and informed while
     putting out the best possible product.</p>
 <p>Thanks for stopping by!</p>
-<p>There is also a link to my LinkedIn account at the bottom of the page.</p>
+<p>There is also a link to my LinkedIn account at the bottom of the page where you can contact me.</p>
+
+
 <?php
 include('includes/footer.html');
 ?>
